2b08ab060c106bbab71678a43e2037ad982e5b53
Michal Žamboch <michal.zamboch@thermofisher.com>
2024-04-19 14:45:11 +0200

    Scroll layout.

--- ui-egui/src/bootstrap.rs
+++ ui-egui/src/bootstrap.rs
@@ -56,43 +56,74 @@ impl Default for MyApp {
     }
 }
 
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+impl MyApp {
+    pub fn insert_test(&mut self) {
+        let item = ToDoItem {
+            checked: false,
+            title: "".into(),
+        };
+        self.todos.push(item);
+    }
+
+    fn create_header(&mut self, ctx: &egui::Context) {
+        egui::TopBottomPanel::top("Header").show(ctx, |ui| {
+            ui.add_space(5.);
             ui.vertical_centered(|ui| {
                 ui.heading("Todo List");
             });
 
-            ui.vertical(|ui| {
-                for item in self.todos.iter() {
-                    ui.add_space(5.);
+            ui.add_space(5.);
+        });
+    }
 
-                    ui.horizontal(|ui| {
-                        ui.with_layout(Layout::left_to_right(Align::LEFT), |ui| {
-                            let check_btn = Button::new("❌").frame(false);
-                            let check_btn_respose = ui.add(check_btn);
-                            if check_btn_respose.clicked() {
-                                println!("Clicked check box: {}", item.title);
-                            }
+    fn create_vertical_layout(&mut self, ctx: &egui::Context) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                ui.vertical(|ui| {
+                    for item in self.todos.iter_mut() {
+                        ui.add_space(5.);
 
-                            let title_label =
-                                Button::new(item.title.clone()).wrap(true).frame(false);
-                            let title_label_response = ui.add(title_label);
-                            if title_label_response.clicked() {
-                                println!("Clicked label: {}", item.title);
-                            }
+                        ui.horizontal(|ui| {
+                            ui.with_layout(Layout::left_to_right(Align::LEFT), |ui| {
+                                let check_btn = Checkbox::without_text(&mut item.checked);
+                                let check_btn_respose = ui.add(check_btn);
+                                if check_btn_respose.clicked() {
+                                    println!("Clicked check box: {}", item.title);
+                                }
+
+                                let title_label =
+                                    Button::new(item.title.clone()).wrap(true).frame(false);
+                                let title_label_response = ui.add(title_label);
+                                if title_label_response.clicked() {
+                                    println!("Clicked label: {}", item.title);
+                                }
+                            });
+
+                            ui.with_layout(Layout::right_to_left(Align::RIGHT), |ui| {
+                                let label = Label::new("1.1.2024".to_owned());
+                                ui.add(label);
+                            });
                         });
 
-                        ui.with_layout(Layout::right_to_left(Align::RIGHT), |ui| {
-                            let label = Label::new("1.1.2024".to_owned());
-                            ui.add(label);
-                        });
-                    });
-
-                    ui.separator();
-                }
+                        ui.separator();
+                    }
+                });
             });
         });
+    }
+
+    fn create_footer(&mut self, ctx: &egui::Context) {
+        egui::TopBottomPanel::bottom("Footer").show(ctx, |ui| {
+            ui.add_space(5.);
+        });
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        self.create_header(ctx);
+        self.create_vertical_layout(ctx);
+        self.create_footer(ctx);
     }
 
     fn save(&mut self, _storage: &mut dyn eframe::Storage) {}
