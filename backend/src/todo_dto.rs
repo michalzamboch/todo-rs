1d46c1b45166d2c959a671a89c93ecb030578ab8
Michal Žamboch <zamboch.michal@seznam.cz>
2024-04-27 11:52:05 +0200

    Todo type updated.

--- backend/src/todo_dto.rs
+++ backend/src/todo_dto.rs
@@ -2,12 +2,18 @@ use std::error::Error;
 use std::fmt;
 
 use ::serde::*;
+use chrono::*;
 
 #[derive(Debug, Default, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
 pub struct TodoDTO {
     id: u32,
+    folder_id: Option<u32>,
     pub title: String,
+    pub description: String,
     pub completed: bool,
+    pub stared: bool,
+    pub creation_time: DateTime<Local>,
+    pub to_be_done_time: Option<DateTime<Local>>,
 }
 
 impl TodoDTO {
@@ -15,12 +21,17 @@ impl TodoDTO {
         TodoDTO {
             id,
             title: title.to_owned(),
+            creation_time: Local::now(),
             ..Default::default()
         }
     }
 
     pub fn id(&self) -> u32 {
         self.id
+    }
+
+    pub fn folder_id(&self) -> Option<u32> {
+        self.folder_id
     }
 
     pub fn equal_by_id(&self, other: &TodoDTO) -> bool {
@@ -43,8 +54,15 @@ impl TodoDTO {
 impl fmt::Display for TodoDTO {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let result = format!(
-            "ID: {} title: {} completed: {}",
-            self.id, self.title, self.completed
+            "ID: {} Folder ID: {} title: {}\n description: {} completed: {} stared: {}\n created: {} to be done: {}",
+            self.id,
+            self.folder_id.unwrap_or_default(),
+            self.title,
+            self.description,
+            self.completed,
+            self.stared,
+            self.creation_time.to_string(),
+            self.to_be_done_time.unwrap_or_default().to_string(),
         );
         write!(f, "{}", result)
     }
